f57576fc02fe59cd32ebf4d9a02298f6e2683d0f
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-09-10 12:06:33 -0300

    FEATURE: Cards are stored on json; cards are shuffled and showed on screen; STYLE: Cards are responsible. ADDED: New online script for work with japanese charactere

--- scripts/gameInterface.js
+++ scripts/gameInterface.js
@@ -1,13 +1,60 @@
+let kanaJson;
 
-let card = {
-    icon: "",
-    flipped: false
+window.onload = () => {
+    fetch('./assets/kana.json')
+        .then(response => response.json())
+        .then(data => {
+            kanaJson = data;
+            createCards();
+        })
+        .then(startGame())
 }
 
-let hiragana = [
-    
-]
-
 let createCards = () => {
+    let cards = [];
 
+    for (const family of Object.values(kanaJson)) {
+        for (const kana of family) {
+            cards.push(createPairOf(kana));
+        }
+    }
+    cards = cards.flatMap(pair => pair);
+    let a = [];
+    for (let index = 0; index < 30; index++) {
+        a.push(cards[index]);
+        shuffle(a);
+    }
+    for (let i = 0; i < a.length; i++) {
+        document.getElementsByClassName("board")[0].innerHTML +=
+            "<div class='card' data-icon='bootstrap'>" +
+            "   <div class='card-front'>" + a[i].content + "</div>" +
+            "   <div class='card-back'>" + "" + "</div>" +
+            "</div>";
+    }
+}
+
+let createPairOf = (kana) => {
+    return [
+        { id: createId(kana), content: wanakana.toHiragana(kana), flipped: false },
+        { id: createId(kana), content: wanakana.toHiragana(kana), flipped: false }
+    ];
+}
+
+let createId = (kana) => {
+    let num = Math.random() * 1000
+    return kana + parseInt(num);
+}
+
+let startGame = () => {
+}
+
+let shuffle = (list) => {
+    let currentIndex = list.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [list[currentIndex], list[randomIndex]] = [list[randomIndex], list[currentIndex]];
+    }
 }
