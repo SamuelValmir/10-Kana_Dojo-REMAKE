c338c905b543e6a9c595e089847b44b8553051e5
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-09-11 13:03:07 -0300

    FEATURE: Cards now have flip animation.  CHANGED: The cards were inserted in the screen (html) by a html code, but now it is added by javascript; REMOVED: json file

--- scripts/gameInterface.js
+++ scripts/gameInterface.js
@@ -1,36 +1,72 @@
+// Global variables
 let kanaJson;
+
+// Contante variables
+const CARD = "card";
+const CARD_FRONT = "card-front";
+const CARD_BACK = "card-back";
+
 
 window.onload = () => {
     fetch('./assets/kana.json')
         .then(response => response.json())
         .then(data => {
             kanaJson = data;
-            createCards();
+            startGame();
         })
-        .then(startGame())
+}
+
+let startGame = () => {
+    createCards();
 }
 
 let createCards = () => {
     let cards = [];
 
+    // It puts each element/kana from kanaJson to cards array
     for (const family of Object.values(kanaJson)) {
         for (const kana of family) {
             cards.push(createPairOf(kana));
         }
     }
+
+    // Duplicate a card
     cards = cards.flatMap(pair => pair);
+
     let a = [];
     for (let index = 0; index < 30; index++) {
         a.push(cards[index]);
         shuffle(a);
     }
+    let boardElement = document.getElementsByClassName("board")[0];
     for (let i = 0; i < a.length; i++) {
-        document.getElementsByClassName("board")[0].innerHTML +=
-            "<div class='card' data-icon='bootstrap'>" +
-            "   <div class='card-front'>" + a[i].content + "</div>" +
-            "   <div class='card-back'>" + "" + "</div>" +
-            "</div>";
+
+        let cardElement = document.createElement("div");
+        cardElement.id = a[i].id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.content = a[i].content;
+
+        createCardBackFront(cardElement, a[i]);
+
+        cardElement.addEventListener('click', flipCard);
+        boardElement.appendChild(cardElement);
     }
+}
+
+function flipCard() {
+    this.classList.toggle("flip");
+}
+
+let createCardBackFront = (cardElement, card) => {
+    let frontCardElement = document.createElement("div");
+    frontCardElement.classList.add(CARD_FRONT);
+    frontCardElement.innerHTML = card.content;
+
+    let backCardElement = document.createElement("div");
+    backCardElement.classList.add(CARD_BACK);
+
+    cardElement.appendChild(frontCardElement);
+    cardElement.appendChild(backCardElement);
 }
 
 let createPairOf = (kana) => {
@@ -45,14 +81,11 @@ let createId = (kana) => {
     return kana + parseInt(num);
 }
 
-let startGame = () => {
-}
-
 let shuffle = (list) => {
     let currentIndex = list.length;
     let randomIndex = 0;
 
-    while (currentIndex !== 0){
+    while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
         [list[currentIndex], list[randomIndex]] = [list[randomIndex], list[currentIndex]];
