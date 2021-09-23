7ce4c5ae499afbd739fe2df33d3a5205f8c95461
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-09-23 12:03:55 -0300

    CSS: All checkbox and buttons of the modal screen has animations

--- scripts/menuInterface.js
+++ scripts/menuInterface.js
@@ -12,12 +12,14 @@ for (const studySection of studySections) {
                 drawCardsOnReferenceScreen("hiragana");
             } else if (studySection.classList.contains("study-section-flash-card")) {
                 document.querySelector(".modal").style.display = "flex";
+                insertAnimationInModalButtons();
+                insertAnimationInCheckbox();
             } /* else if (studySection.classList.contains("study-section-reference")) {
                 document.querySelector(".menu-screen").style.display = "none";
                 document.querySelector(".reference-screen").style.display = "block";
                 changeScreen(".menu-screen", ".reference-screen");
             }
-
+            
             */
         });
     }
@@ -29,4 +31,36 @@ function goToPlayMenu() {
 
 function goToLearnMenu() {
 
+}
+
+let insertAnimationInModalButtons = () => {
+    let modalButtons = document.querySelectorAll(".modal-bottom div");
+    modalButtons.forEach((modalButton) => {
+        modalButton.addEventListener("click", () => {
+            modalButton.animate([
+                { backgroundColor: "#ddd" },
+                { backgroundColor: "var(--white)" },
+            ], 500)
+        });
+    })
+}
+
+let insertAnimationInCheckbox = () => {
+    let checkBoxList = document.querySelectorAll(".checkbox-container input");
+    checkBoxList.forEach((checkBox) => {
+        checkBox.addEventListener('click', () => {
+
+            let checkMarkCircle = checkBox.parentElement.children[1];
+            checkMarkCircle.animate([
+                { opacity: 1 },
+                { transform: "scale(3.5)" }
+            ], 200);
+
+            let checkMark = checkBox.parentElement.children[2];
+            checkMark.animate([
+                { transform: "scale(0.8)" }
+            ], 200)
+        });
+
+    });
 }
