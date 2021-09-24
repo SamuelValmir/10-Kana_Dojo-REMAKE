ab52b31827f5b3d5b76bc51a5fbca8ffc5d33b26
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-09-24 09:59:47 -0300

    STYLE: Flash card modal is responsive for cellphone; FEATURE: Some options of the flash cards modal have edit icon that appears if checkbox is checked

--- scripts/menuInterface.js
+++ scripts/menuInterface.js
@@ -12,6 +12,7 @@ for (const studySection of studySections) {
                 drawCardsOnReferenceScreen("hiragana");
             } else if (studySection.classList.contains("study-section-flash-card")) {
                 document.querySelector(".modal").style.display = "flex";
+                setCheckboxEditIcon();
                 insertAnimationInModalButtons();
                 insertAnimationInCheckbox();
             } /* else if (studySection.classList.contains("study-section-reference")) {
@@ -50,6 +51,11 @@ let insertAnimationInCheckbox = () => {
     checkBoxList.forEach((checkBox) => {
         checkBox.addEventListener('click', () => {
 
+            // If the check box is inside of modal content right...
+            if (checkBox.parentElement.parentElement.parentElement.classList.contains("modal-content-right")) {
+                showEditIcon(checkBox);
+            }
+
             let checkMarkCircle = checkBox.parentElement.children[1];
             checkMarkCircle.animate([
                 { opacity: 1 },
@@ -63,4 +69,30 @@ let insertAnimationInCheckbox = () => {
         });
 
     });
+}
+
+let setCheckboxEditIcon = () => {
+    let checkBoxEditIconList = document.querySelectorAll(".modal-content-right span img");
+    for (let i = 0; i < checkBoxEditIconList.length; i++) {
+        let checkBoxEditIcon = checkBoxEditIconList[i];
+        if (i == 0) {
+            checkBoxEditIcon.style.display = "block";
+        } else {
+            checkBoxEditIcon.style.display = "none";
+        }
+        checkBoxEditIcon.addEventListener("click", () => {
+            console.log(checkBoxEditIcon)
+        })
+    }
+
+}
+
+// It shows the edit icon when check box is checked
+let showEditIcon = (checkBox) => {
+    let checkBoxEditIcon = checkBox.parentElement.parentElement.children[1];
+    if (checkBoxEditIcon.style.display === "none") {
+        checkBoxEditIcon.style.display = "block"
+    } else if (checkBoxEditIcon.style.display === "block") {
+        checkBoxEditIcon.style.display = "none"
+    }
 }
