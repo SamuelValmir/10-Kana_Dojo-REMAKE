cd753b0c3516f4540c2bc09acc64c595b727819f
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-09-30 12:47:55 -0300

    #1CheckmarkOfTheModalCustomIsNotOverflowing FEATURES: Animation for the checkboxes of the modal custom; Animation for the edit icon of the checkbox of the modal main;

--- scripts/menuInterface.js
+++ scripts/menuInterface.js
@@ -7,7 +7,9 @@ let checkBoxList;
 let checkBoxLeftList;
 let checkBoxRightList;
 
+let modalCustomCheckBoxList;
 let modalButtons;
+
 
 let studySections = document.querySelectorAll(".menu-screen .container .study-section");
 
@@ -42,7 +44,7 @@ function goToLearnMenu() {
 }
 
 let setModalButtons = () => {
-    modalButtons = document.querySelectorAll(".modal-bottom div");
+    modalButtons = document.querySelectorAll(".modal-bottom .modal-button");
 }
 
 let insertAnimationInModalMainCheckbox = () => {
@@ -61,7 +63,7 @@ let insertAnimationInModalMainCheckbox = () => {
             checkBox.style.borderColor = MENU_PRIMARY_COLOR;
             checkMark.style.display = "block"
         }
-    
+
         let hideCheckBoxAndCheckMark = () => {
             checkBox.dataset.checked = "false";
             checkBox.style.backgroundColor = "rgb(0, 0, 0, 0)";
@@ -85,9 +87,6 @@ let insertAnimationInModalMainCheckbox = () => {
             checkBox.style.backgroundColor = MENU_PRIMARY_COLOR;
             checkBox.style.borderColor = MENU_PRIMARY_COLOR;
             checkMark.style.display = "block"
-        } else {
-            checkBox.dataset.checked = "false";
-            checkMark.style.display = "none"
         }
 
         checkBox.addEventListener('click', () => {
@@ -116,9 +115,10 @@ let insertAnimationInModalMainCheckbox = () => {
             let lastCheckBoxCheckLeft = checkBoxLeftList.find((e) => e.dataset.checked === "true");
             let lastCheckBoxCheckRight = checkBoxRightList.find((e) => e.dataset.checked === "true");
 
-            // console.log(checkBox.dataset.checked)
-            // console.log(checkBoxLeftList)
             if (num == 1 && (lastCheckBoxCheckLeft === checkBox || lastCheckBoxCheckRight === checkBox)) {
+                setTimeout(() => {
+                    alert("At least one option must be selected!");
+                }, 200);
             } else {
                 if (checkBox.dataset.checked === "true") {
                     console.log("hide")
@@ -142,6 +142,87 @@ let insertAnimationInModalMainCheckbox = () => {
         });
     }
 }
+
+let insertAnimationInModalCustomCheckbox = () => {
+
+    // checkBoxList = document.querySelectorAll(".checkbox-container .checkbox");
+
+    // It converts a NodeList object to an array.
+    // It works well in all modern and old browsers including IE 6+.
+    modalCustomCheckBoxList = Array.prototype.slice.call(modalCustomCheckBoxList);
+    for (let i = 0; i < modalCustomCheckBoxList.length; i++) {
+        let checkBox = modalCustomCheckBoxList[i];
+        let checkMarkCircle = checkBox.parentElement.children[0];
+        let checkMark = checkBox.children[0];
+
+        let showCheckBoxAndCheckMark = () => {
+            checkBox.dataset.checked = "true";
+            checkBox.style.backgroundColor = MENU_PRIMARY_COLOR;
+            checkBox.style.borderColor = MENU_PRIMARY_COLOR;
+            checkMark.style.display = "block"
+        }
+
+        let hideCheckBoxAndCheckMark = () => {
+            checkBox.dataset.checked = "false";
+            checkBox.style.backgroundColor = "rgb(0, 0, 0, 0)";
+            checkBox.style.borderColor = "rgba(0, 0, 0, 0.6)";
+            checkMark.style.display = "none"
+        }
+
+        // It is for always reset the values when 
+        hideCheckBoxAndCheckMark();
+
+        if (i === 0) {
+            checkBox.dataset.checked = "true";
+            checkBox.style.backgroundColor = MENU_PRIMARY_COLOR;
+            checkBox.style.borderColor = MENU_PRIMARY_COLOR;
+            checkMark.style.display = "block"
+        }
+
+        checkBox.addEventListener('click', () => {
+            checkMarkCircle.animate([
+                { opacity: 1 },
+                { transform: "scale(3.5)" }
+            ], 200);
+
+            checkBox.animate([
+                { transform: "scale(0.8)" }
+            ], 200);
+
+            modalCustomCheckBoxList.filter = filter;
+            checkBoxRightList.filter = filter;
+
+            let num = modalCustomCheckBoxList.filter(isChecked);
+            let modalCustomLastCheckBoxCheck = modalCustomCheckBoxList.find((e) => e.dataset.checked === "true");
+
+            if (num == 1 && (modalCustomLastCheckBoxCheck === checkBox)) {
+                setTimeout(() => {
+                    alert("At least one option must be selected!");
+                }, 200);
+            } else {
+                if (checkBox.dataset.checked === "true") {
+                    console.log("hide")
+                    hideCheckBoxAndCheckMark();
+                    // It show the edit icon only if check box is inside of modal content right
+                    if (checkBox.parentElement.parentElement.parentElement.classList.contains("modal-content-right")) {
+                        showEditIcon(checkBox, false);
+                    }
+
+
+                } else if (checkBox.dataset.checked === "false") {
+                    console.log("show")
+                    showCheckBoxAndCheckMark();
+                    // It show the edit icon only if check box is inside of modal content right
+                    if (checkBox.parentElement.parentElement.parentElement.classList.contains("modal-content-right")) {
+                        showEditIcon(checkBox, true);
+                    }
+                }
+
+            }
+        });
+    }
+}
+
 
 // It shows the edit icon when check box is checked
 
@@ -171,13 +252,14 @@ let insertAnimationInModalButtons = () => {
 }
 
 let executeModalButton = (modalButton) => {
-    if (modalButton.innerText === "BACK") {
+    if (modalButton.classList.contains("modal-main-button-back")) {
         modalMain.style.display = "none";
-    } else if (modalButton.innerText === "START") {
+    } else if (modalButton.classList.contains("modal-main-button-start")) {
         modalMain.style.display = "none";
-    } else if (modalButton.innerText === "CANCEL") {
+        //To show flash card screen
+    } else if (modalButton.classList.contains("modal-custom-button-cancel")) {
         modalCustom.style.display = "none";
-    } else if (modalButton.innerText === "FINISH") {
+    } else if (modalButton.classList.contains("modal-custom-button-finish")) {
         modalCustom.style.display = "none";
     }
 }
@@ -186,29 +268,35 @@ let setCheckboxEditIcon = () => {
     //It storage all Check Boxes that are in the right content of the modal-main.
     let checkBoxEditIconList = document.querySelectorAll(".modal-main .modal-content-right span img");
     for (let i = 0; i < checkBoxEditIconList.length; i++) {
-        let checkBoxEditIcon = checkBoxEditIconList[i];
+        const checkBoxEditIcon = checkBoxEditIconList[i];
         // The first edit icon will appears by default
         if (i == 0) {
             checkBoxEditIcon.style.display = "block";
         } else {
             checkBoxEditIcon.style.display = "none";
         }
-        checkBoxEditIcon.addEventListener("click", () => {
-            insertAnimationInModalCustomCheckbox();
+        checkBoxEditIcon.addEventListener("click", async() => {
+            await animateEditIcon(checkBoxEditIcon);
             fillModalCustomContent(checkBoxEditIcon);
+            insertAnimationInModalCustomCheckbox();
         })
     }
 
 }
 
-let insertAnimationInModalCustomCheckbox = () => {
-    let a = document.querySelectorAll(".modal-custom .checkBox");
+let animateEditIcon = (checkBoxEditIcon) => {
 
-    for (let i = 0; i < a.length; i++) {
-        const element = a[i];
+    let promise = new Promise ((resolve) =>{
+        let checkBoxEditIconAnimation = checkBoxEditIcon.animate([
+            {transform: "scale(.8)"}
+        ], 150);
+    
+        checkBoxEditIconAnimation.addEventListener('finish', () => {
+            resolve();
+        })
+    })
 
-
-    }
+  return promise;
 }
 
 let fillModalCustomContent = (checkBoxEditIcon) => {
@@ -220,10 +308,11 @@ let fillModalCustomContent = (checkBoxEditIcon) => {
     modalCustomTopText.innerHTML = "Custom " + checkBoxEditIcon.name;
 
     let modalCustomContent = document.querySelector(".modal-custom .modal-content");
-    modalCustomContent.innerHTML = "";
+    // modalCustomContent.innerHTML = "";
 
     let group;
 
+    // It defines which group it will be displayed in modal custom
     switch (checkBoxEditIcon.name) {
         case "Basic":
             group = kanaBasic;
@@ -268,14 +357,24 @@ let fillModalCustomContent = (checkBoxEditIcon) => {
 let insertFamilies = (group, alphabet, modalCustomContent) => {
 
     // It makes the content of the Modal Custom
-    for (const family of Object.values(group)) {
+    for (let i = 0; i < Object.values(group).length; i++) {
+        const family = Object.values(group)[i];
 
         let familyElement = document.createElement("span");
         familyElement.classList.add("family");
-
+        familyElement.style.name = Object.keys(group)[i];
+        familyElement.style.position = "relative";
+        familyElement.setAttribute("alphabet", alphabet);
+        
+        let checkMarkCircleElement = document.createElement("span");
+        checkMarkCircleElement.classList.add("checkmark-circle");
+        
         let checkBoxElement = document.createElement("span");
         checkBoxElement.classList.add("checkbox");
-
+        
+        let checkMarkElement = document.createElement("span");
+        checkMarkElement.classList.add("checkmark");
+        
         let familyContentElement = document.createElement("span");
         familyContentElement.style.display = "flex";
         familyContentElement.style.gap = "10px"
@@ -292,9 +391,14 @@ let insertFamilies = (group, alphabet, modalCustomContent) => {
         }
 
         modalCustomContent.appendChild(familyElement);
+        familyElement.appendChild(checkMarkCircleElement);
         familyElement.appendChild(checkBoxElement);
         familyElement.appendChild(familyContentElement);
+        checkBoxElement.appendChild(checkMarkElement);
+        console.log(familyElement.getAttribute("alphabet"));
+        // console.log(familyContentElement);
     }
+    modalCustomCheckBoxList = document.querySelectorAll(".modal-custom .checkbox");
 }
 
 let isChecked = e => e.dataset.checked === "true";
